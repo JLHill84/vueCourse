149dccb463f42c6011190b4480f6bbe63d3a10fe
joshua.hill@slalom.com-at-580417612588 <joshua.hill@slalom.com>
2020-08-07 16:25:22 -0500

    filters and mixins

---
+++ filtersAndMixins/src/List.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>Filters & Mixins</h1>
+    <hr />
+    <input v-model="filterText" type="text" />
+    <ul>
+      <li v-for="fruit in filteredFruits" :key="fruit.id">
+        {{ fruit }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { fruitMixin } from "./fruitMixin.js";
+
+export default {
+  mixins: [fruitMixin],
+};
+</script>
+
+<style></style>
