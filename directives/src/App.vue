a3e719f77ac1c0f52cf4027f0566499d1de83a93
joshua.hill@slalom.com-at-580417612588 <joshua.hill@slalom.com>
2020-08-07 14:37:53 -0500

    first

---
+++ directives/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives</h1>
+        <p v-highlight:background.delayed="'blue'">color funs</p>
+        <p v-local-highlight:background.delayed.blink="'grey'">
+          color funs local
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        let delay = 0;
+        if (binding.modifiers["delayed"]) {
+          delay = 3000;
+        }
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value;
+          let secondColor = "yellow";
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == secondColor
+                ? (currentColor = mainColor)
+                : (currentColor = secondColor);
+              if (binding.arg == "background") {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+            }, 2000);
+          }, delay);
+        } else {
+          setTimeout(() => {
+            if (binding.arg == "background") {
+              el.style.backgroundColor = binding.value;
+            } else {
+              el.style.color = binding.value;
+            }
+          }, delay);
+        }
+      },
+    },
+  },
+};
+</script>
+
+<style></style>
